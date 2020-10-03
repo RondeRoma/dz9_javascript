37f9f2be8f263468e0f080c9f6e959d5943746af
Roman <4895476@gmail.com>
2020-10-03 22:11:40 +0300

    Добавляет вторую домашку по JS

---
+++ js/dz2-4.js
@@ -0,0 +1,15 @@
+// function formatString(string, maxLength = 40) {
+//   return string.length <= maxLength
+//     ? string
+//     : string.slice(0, maxLength) + '...';
+// }
+// console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
+// // 'Curabitur ligula sapien, tincidunt non.'
+
+// console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
+// // 'Vestibulum facilisis, purus nec pulvinar...'
+
+// console.log(
+//   formatString('Vestibulum facilisis, purus nec pulvinar iaculis.', 20),
+// );
+// // 'Vestibulum facilisis, purus ne...'
---
+++ js/dz2-5.js
@@ -0,0 +1,13 @@
+// function checkForSpam(str) {
+//   'use strict';
+//   const findWord =
+//     str.toLowerCase().includes('sale') || str.toLowerCase().includes('spam');
+//   return findWord;
+// }
+// console.log(checkForSpam('Latest technology news')); // false
+
+// console.log(checkForSpam('JavaScript weekly newsletter')); // false
+
+// console.log(checkForSpam('Get best sale offers now!')); // true
+
+// console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
