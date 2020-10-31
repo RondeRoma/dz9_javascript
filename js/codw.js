db83d88d8752ee09597c0990e371d285af6a6806
Roman <4895476@gmail.com>
2020-10-31 23:08:45 +0200

    добавляет 6ю домашку

--- js/codw.js
+++ js/codw.js
@@ -1,5 +1,29 @@
-function isTriangle(a, b, c) {
-  return a + b >= c && a + c >= b && b + c >= a;
-}
-console.log(isTriangle(1, 2, 2));
-console.log(isTriangle(7, 2, 2));
+// function isTriangle(a, b, c) {
+//   return a + b >= c && a + c >= b && b + c >= a;
+// }
+// console.log(isTriangle(1, 2, 2));
+// console.log(isTriangle(7, 2, 2));
+
+
+
+
+// // Задача про Адам и Еву
+// class Human {}
+// class Man extends Human {}
+// class Woman extends Human {}
+
+// class God {
+//   static create() {
+//     const adan = new Man()
+//     const eva = new Woman()
+
+//     return [adan, eva]
+//   }
+// }
+
+// const playerRankUp = points =>
+//   points >= 100
+//     ? 'Well done! You have advanced to the qualifying stage. Win 2 out of your next 3 games to rank up.'
+//     : false
+
+
